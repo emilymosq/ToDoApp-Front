8e69ae8f2859c25469bd7bede1f30f120b9dbcf2
emilymsq <emilymosq4@gmail.com>
2024-11-26 21:15:53 +0100

    conflictos arreglados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,21 +14,13 @@ import { NavbarBottomComponent } from './navbar-bottom/navbar-bottom.component';
 import { NotasBusquedaComponent } from './notas-busqueda/notas-busqueda.component';
 import { NuevaNotaComponent } from './nueva-nota/nueva-nota.component';
 import { RegistroComponent } from './registro/registro.component';
-
-import localePy from '@angular/common/locales/es-PY';
-import {registerLocaleData} from "@angular/common";
 import { ConfiguracionComponent } from './configuracion/configuracion.component';
-<<<<<<< HEAD
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import { BotonExamenComponent } from './boton-examen/boton-examen.component';
 import { ListaTareasExamenComponent } from './lista-tareas-examen/lista-tareas-examen.component';
 import { DetalleTareaExamenComponent } from './detalle-tarea-examen/detalle-tarea-examen.component';
 import { FormularioComponent } from './formulario/formulario.component';
 import { TrabajandoConArraysComponent } from './trabajando-con-arrays/trabajando-con-arrays.component';
-=======
-import {ReactiveFormsModule} from "@angular/forms";
->>>>>>> d9d3d599b1aaeb78e777718cd0f427f686c3d9bc
-registerLocaleData(localePy, 'es');
 
 @NgModule({
   declarations: [
@@ -43,27 +35,20 @@ registerLocaleData(localePy, 'es');
     NotasBusquedaComponent,
     NuevaNotaComponent,
     RegistroComponent,
-<<<<<<< HEAD
     ConfiguracionComponent,
     BotonExamenComponent,
     ListaTareasExamenComponent,
     DetalleTareaExamenComponent,
     FormularioComponent,
-    TrabajandoConArraysComponent
-=======
-    ConfiguracionComponent
->>>>>>> d9d3d599b1aaeb78e777718cd0f427f686c3d9bc
+    TrabajandoConArraysComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     NgbModule,
-<<<<<<< HEAD
       ReactiveFormsModule,
-      FormsModule
-=======
+      FormsModule,
       ReactiveFormsModule
->>>>>>> d9d3d599b1aaeb78e777718cd0f427f686c3d9bc
   ],
   providers: [{
     provide: LOCALE_ID, useValue: 'es'
